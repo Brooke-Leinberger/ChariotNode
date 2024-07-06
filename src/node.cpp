69300d553fd5675924e25dc2692af6444b64a281
Brooke Juno Leinberger <brooke.j.leinberger@gmail.com>
2024-07-06 12:30:14 -0400

    Testing log levels

--- src/node.cpp
+++ src/node.cpp
@@ -3,6 +3,13 @@
 
 int main()
 {
-    LOG_info("Starting Chariot...");
-    LOG_info("Stopping Chariot...");
+    LOG_fatal("Fatal error");
+    LOG_error("Standard error");
+    LOG_IEC();
+    LOG_warn("Warning");
+    LOG_info("Information");
+    LOG_verbose(0, "A little extra info");
+    LOG_verbose(1, "Debugging");
+    LOG_verbose(2, "A lot of debugging");
+    LOG_verbose(9, "Dear god, the debugging");
 }
