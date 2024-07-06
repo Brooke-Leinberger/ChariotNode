22b04cffc51b718e41136cf062631ea66858f1a0
Brooke Juno Leinberger <brooke.j.leinberger@gmail.com>
2024-07-06 14:05:58 -0400

    Added color, adjusted error names

--- src/node.cpp
+++ src/node.cpp
@@ -3,13 +3,13 @@
 
 int main()
 {
-    LOG_fatal("Fatal error");
-    LOG_error("Standard error");
+    LOG_FATAL("Fatal error");
+    LOG_ERROR("Standard error");
     LOG_IEC();
-    LOG_warn("Warning");
-    LOG_info("Information");
-    LOG_verbose(0, "A little extra info");
-    LOG_verbose(1, "Debugging");
-    LOG_verbose(2, "A lot of debugging");
-    LOG_verbose(9, "Dear god, the debugging");
+    LOG_WARN("Warning");
+    LOG_INFO("Information");
+    LOG_VERBOSE(0, "A little extra info");
+    LOG_VERBOSE(1, "Debugging");
+    LOG_VERBOSE(2, "A lot of debugging");
+    LOG_VERBOSE(9, "Dear god, the debugging");
 }
